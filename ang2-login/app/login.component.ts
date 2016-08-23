e6095e340b282f5842505b1577331cd2663a5c73
cuongdv <cuong.duong@htklabs.com>
2016-08-23 18:23:31 +0700

    update

---
+++ ang2-login/app/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'login',
+	templateUrl: 'app/login.html',
+	styleUrls: ['app/login-style.css']
+})
+export class LoginComponent implements OnInit {
+
+	constructor() {}
+
+	ngOnInit(): void {
+		
+	}
+}
